92e04a17f2aa89959a8f5c6fd4be9fe073958301
vishnupriy-a <116629496+vishnupriy-a@users.noreply.github.com>
2022-11-18 15:38:18 +0530

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,14 @@
+def fib(x):
+  a=0
+  b=1
+  print(a)
+  print(b)
+  for i in range(1,x+1):
+    c=a+b
+    print(c)
+    a=b
+    b=c
+num=int(input("\n Enter the number:"))
+print("\n Fibonocci series...................\n")
+fib(num)
+    
