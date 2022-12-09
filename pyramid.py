e8cc74f6802d6ea9448bb99e3543e16b4906089e
vishnupriy-a <116629496+vishnupriy-a@users.noreply.github.com>
2022-12-09 06:30:31 +0530

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,11 +1,10 @@
 def pyramid(n):
-	print("enter the pyramid:")
-	for i in range(1,n+1):
-		space=' '
-		for j in range(1,i+1):
-			space=space+str(i*j)+" "
-		print(space)
-n=int(input("enter the number:"))
+    print(" pyramid is:")
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(i*j, end=" ")
+        print("\n")
+n=int(input("enter the number of lines:"))
 pyramid(n)
  
 
